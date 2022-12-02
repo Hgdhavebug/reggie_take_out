6d54b311d8dfbc655fd5825855d5ab742c3486c9
huguidong <3@126.com>
2022-12-02 18:17:01 +0800

    全局异常处理部分

--- src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -1,8 +1,32 @@
 package com.itheima.reggie.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
+ *全局异常处理
  * @author HGD
  * @create 2022-12-02 17:05
  */
+@ControllerAdvice
+@ResponseBody
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+
 }
