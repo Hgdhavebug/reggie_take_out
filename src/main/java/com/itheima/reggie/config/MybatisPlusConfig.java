61b596c31e9c20af5e57a7b15f7e28607294742d
Administrator <2548087374@qq.com>
2022-12-09 13:29:28 +0800

    全局异常处理部分

---
+++ src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.itheima.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author HGD
+ * @create 2022-12-06 18:29
+ * 配置MP的分分页插件
+ */
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
