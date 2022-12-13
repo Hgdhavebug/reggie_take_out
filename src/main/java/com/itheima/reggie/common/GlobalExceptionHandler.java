8664f82650c805b33e3e1b1c0af04fd91323423d
Administrator <2548087374@qq.com>
2022-12-13 19:05:25 +0800

    戴尔重新部署 修改mysql配置

--- src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
- *全局异常处理
+ * 全局异常处理
+ *
  * @author HGD
  * @create 2022-12-02 17:05
  */
@@ -18,18 +19,14 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
         log.error(ex.getMessage());
 
-        if (ex.getMessage().contains("Duplicate entry")){
+        if (ex.getMessage().contains("Duplicate entry")) {
             String[] split = ex.getMessage().split(" ");
             String msg = split[2] + "已存在";
             return R.error(msg);
         }
         return R.error("未知错误");
     }
-
-
-
-
 }
